55092529532b6e91cd5ef13878e8d4dff34d3690
Atif Ali <94118166+atifali1307@users.noreply.github.com>
2023-10-20 18:13:19 +0530

    Add files via upload

---
+++ GFG 20-10-23.java
@@ -0,0 +1,9 @@
+class Solution {
+    static int isPossible(int N, int arr[]) {
+        int rem=0;
+        for(int i:arr){
+            rem+=i%3;
+        }
+        return rem%3==0?1:0;
+    }
+}
